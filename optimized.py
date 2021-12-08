5887a08d6c21d32b953af1cbe8c179273b6debde
AkaSan242 <Lucaka@icloud.com>
2021-12-09 00:35:05 +0100

    Création de la fonction create_csv et formatage de l'affichage du bénéfice et du budget

--- optimized.py
+++ optimized.py
@@ -1,8 +1,9 @@
+import csv
+import math
 from optimuslib import get_action
 from memory_profiler import profile
 
 
-@profile
 def most_benefice_optimized(budget, actions):
     """Create a table with a line for the budget and a line for actions"""
     table = [[0 for x in range(budget + 1)] for x in range(len(actions) + 1)]
@@ -14,7 +15,7 @@ def most_benefice_optimized(budget, actions):
             action = actions[i-1]
 
             #to make sure we don't exceed the budget
-            if action.price <= b: 
+            if action.real_price <= b:
                 #if the benefice is better than the previous one the new benefice become the max 
                 table[i][b] = max(action.benefice + table[i-1][b-action.price], table[i-1][b])
             else:
@@ -27,7 +28,7 @@ def most_benefice_optimized(budget, actions):
     taken_actions = []
 
     #while we still have money on budget and there is still actions to check 
-    while b >= 0 and a >= 0:
+    while b >= 1 and a >= 0:
         e = actions[a-1]
 
         #if the action is the max benefice in the table for the budget we take it
@@ -37,18 +38,45 @@ def most_benefice_optimized(budget, actions):
             
 
         a -= 1
-        
+
+    spend_budget = sum([i.real_price for i in taken_actions])
+    formated_spend_budget = "{:.2f}".format(spend_budget)
     
-    return (["bénéfice: " + str(table[-2][-2]) + "€", "Budget dépensé: " + str(500 - b) + "€"] ,
-     ["Liste des actions à acheter: " + str([i.name for i in taken_actions])]
+    
+    most_benefice = table[-2][-2]
+    formated_benefice = "{:.2f}".format(most_benefice)
+
+    create_csv('dataset1report.csv', taken_actions, formated_spend_budget, formated_benefice)
+
+    return (["bénéfice: " + str(most_benefice) + "€", "Budget dépensé: " + str(formated_spend_budget) + "€"] ,
+     ["Liste des actions à acheter: " + str([i.name for i in taken_actions]) + " " + "Nombre d'actions: " +  str(len(taken_actions))]
     )
+
+def create_csv(csvfile, list, budget, benefice):
+    
+    with open(csvfile, 'w', newline='', encoding='utf-8-sig') as data:
+                datawriter = csv.writer(data, delimiter=',', dialect='excel', quoting=csv.QUOTE_ALL)
+
+                datawriter.writerow(['Name', 'Price', 'Bénéfice'])
+
+    for elt in list:
+            with open(csvfile, 'a', newline='', encoding='utf-8-sig') as data:
+                datawriter = csv.writer(data, delimiter=',', dialect='excel')
+                datawriter.writerow([elt.name, str(elt.real_price) + "€", str(elt.benefice) + "%"])
+
+    with open(csvfile, 'a', newline='', encoding='utf-8-sig') as data:
+                datawriter = csv.writer(data, delimiter=',', dialect='excel')
+                datawriter.writerow(["Total bénéfice: " + str(benefice) + "€" + " " + "Budget dépensé: " + str(budget) + "€"
+                + " " + "Nombre d'action: " + str(len(list))])
+
 
 
 customer_budget = 500
 actions_list = []
 
+
 if __name__=='__main__':
-    f = open(r"actions.csv")
+    f = open(r"dataset1.csv")
     get_action(f, actions_list)
     print("Voici le meilleur plan d'investissement:")
     print(most_benefice_optimized(customer_budget, actions_list))
