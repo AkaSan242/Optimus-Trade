c6c9ff3cac27919e2cba831c613f8f2040beeb63
AkaSan242 <Lucaka@icloud.com>
2021-12-09 00:43:06 +0100

    Optimus Trade Application

---
+++ OptimusTrade/optimuslib.py
@@ -0,0 +1,29 @@
+"""Optimus trade lib"""
+import csv
+import math
+
+
+class Action:
+    def __init__(self, name, price, profit):
+
+        self.name = name
+        self.price = math.ceil(float(price))
+        self.real_price = float(price)
+        self.profit = float(profit)
+        self.percentage = self.price * self.profit / 100
+        self.benefice = float("{:.2f}".format(self.percentage))
+
+    def __repr__(self):
+        return (
+            f"Action: '{self.name}' Prix: {self.price}€"
+            f" Pourcentage(2ans): {self.profit}% Bénéfice: {self.benefice}€"
+        )
+
+  
+def get_action(file, list):
+    """Read a csv file, take the data and transform them in actions to use them after"""
+    read = csv.DictReader(file)
+    for row in read:
+        action = Action(row["name"], row["price"], row["profit"])
+        if action.price > 0:
+            list.append(action)
